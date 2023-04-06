d0c248007fc15d9969211a5e6f0a9d44f5717fb1
DubanStevenCastilloMendoza <estivencastmen@gmail.com>
2023-04-05 20:47:42 -0500

    ejercicio apostoles completo

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function apostoles() {
+    let numero = prompt("ingrese un numero");
+    let apostol = [
+      "",
+      "Pedro",
+      "Andres",
+      "Santiago",
+      "Juan",
+      "Jacobo",
+      "Simón",
+      "Judas Tadeo",
+      "Judas Iscariote",
+      "Natanael",
+      "Mateo",
+      "Felipe",
+      "Tomas",
+    ];
+    console.log(apostol[numero]);
+  }
+  
